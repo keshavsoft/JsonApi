8c201657f4419410489ef019e49686b8734798b1
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-04-01 16:12:58 +0530

    keshav

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Items.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Items.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 let CommonScreens = require("./Items/Screens");
+let CommonImages = require("./Items/Images");
 
 router.use('/Screens', CommonScreens);
+router.use('/Images', CommonImages);
 
 module.exports = router;
