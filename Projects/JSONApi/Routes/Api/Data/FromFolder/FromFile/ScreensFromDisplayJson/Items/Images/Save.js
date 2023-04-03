7106531e405d34aa515c38a91824d7133016bed9
keshavsoft <nknnkumar@yahoo.com>
2023-04-04 00:01:42 +0530

    keshav

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Items/Images/Save.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Items/Images/Save.js
@@ -1,8 +1,31 @@
 let express = require("express");
 let router = express();
+const multer = require('multer');
+var path = require('path');
+//let DataSu = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/Images/UsingMulter");
+let DataSu = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/JsonFileAsFolder/ItemNameAsFolder/ImagesFolder/InsertToFolder/UsingMulter");
 
-router.post('/', (req, res) => {
-    console.log(req.file, req.body);
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "Images");
+    },
+    filename: function (req, file, cb) {
+        console.log("req : ", req.KeshavSoft, file);
+        //  let ext = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+        //cb(null, Date.now() + ext);
+        //  cb(null, Date.now() + path.extname(file.originalname));
+        //cb(null, req.body.nspeakers + path.extname(file.originalname));
+
+        // cb(null, req.KeshavSoft + path.extname(file.originalname));
+        cb(null, file.originalname);
+
+    }
+});
+
+var upload = multer({ storage: storage });
+
+router.post('/', DataSu.upload.single("uploaded_file"), (req, res) => {
+    console.log("1111111111111 : ", req.KeshavSoft, req.body);
     res.end("success");
 });
 
