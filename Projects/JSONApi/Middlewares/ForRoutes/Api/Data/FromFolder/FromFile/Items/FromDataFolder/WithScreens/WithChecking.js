8ae8892834e02f53b87fd7b47635e92c1664c076
JatinNeeraj <jatinucm123@gmail.com>
2023-04-05 16:53:34 +0530

    jnn

---
+++ Projects/JSONApi/Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/WithScreens/WithChecking.js
@@ -0,0 +1,46 @@
+exports.CreateNewFunc = (req, res, next) => {
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                FolderName: {},
+                FileNameOnly: {},
+                ItemName: {},
+                insertkey: "",
+            },
+            originalUrl: req.originalUrl
+        });
+    } else {
+        if (("KeshavSoft" in req) === false) {
+            res.json({ KTF: false, KReason: "KeshavSoft not found in Request" })
+        } else {
+            if (("DataPk" in req.KeshavSoft) === false) {
+                res.json({ KTF: false, KReason: "DataPk not found in KeshavSoft" })
+
+            } else {
+                if (("FolderName" in req.body) === false) {
+                    res.json({ KTF: false, KReason: "FolderName not found in body" })
+                } else {
+                    if (("FileNameOnly" in req.body) === false) {
+                        console.log("eeeeeeeeee : ", "FileNameOnly" in req.body);
+                        res.json({ KTF: false, KReason: "ItemConfig not found in body" });
+                        res.end();
+                    } else {
+                        if (("ItemName" in req.body) === false) {
+                            res.json({ KTF: false, KReason: "ItemName not found in body" })
+
+                        } else {
+                            if (("ScreenName" in req.body) === false) {
+                                res.json({ KTF: false, KReason: "ScreenName not found in body" })
+
+                            } else {
+                                next();
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+}; 
