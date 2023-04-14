fc26133bee2850a155f99792a490009659d6f011
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-04-14 08:24:48 +0530

    keshav

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/WithScreens/WithChecking.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/WithScreens/WithChecking.js
@@ -2,8 +2,9 @@ let express = require('express');
 let router = express.Router();
 //let Repos = require("../../../../../../../Repository/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AsArrayWithPK");
 let CommonConrollers = require("../../../../../../../../controllers/Api/Data/FromFolder/FromFile/Items/FromDataFolder/WithScreens/WithChecking.controller");
+let CommonMiddlewares = require("../../../../../../../../Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/WithScreens/WithChecking");
 
-router.post('/CreateNew', CommonConrollers.CreateNewFunc);
+router.post('/CreateNew', CommonMiddlewares.CreateNewFunc, CommonConrollers.CreateNewFunc);
 router.post('/Insert', CommonConrollers.InsertFunc);
 router.post('/', CommonConrollers.PostFunc);
 router.delete('/', CommonConrollers.DeleteFunc);
