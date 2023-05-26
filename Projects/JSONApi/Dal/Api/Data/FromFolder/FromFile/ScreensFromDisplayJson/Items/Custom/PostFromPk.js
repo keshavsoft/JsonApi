d5dba9ee118041edfef654137243dfb39e1b1546
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-05-26 16:19:08 +0530

    SREENIVAS

--- Projects/JSONApi/Dal/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Items/Custom/PostFromPk.js
+++ Projects/JSONApi/Dal/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Items/Custom/PostFromPk.js
@@ -1,7 +1,7 @@
 let CommonDataSupply = require("../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/CustomFuncs/AutoPost");
 
-exports.GetFunc = async ({ inClientName, inPurchasePK }) => {
-   let LocalDataToReturn = await CommonDataSupply.StartFunc({inClientName, inPurchasePK });
+exports.GetFunc = async ({ inClientName, inPurchasePK,inDataPk }) => {
+   let LocalDataToReturn = await CommonDataSupply.StartFunc({inClientName, inPurchasePK ,inDataPk});
    return await LocalDataToReturn;
 };
 
