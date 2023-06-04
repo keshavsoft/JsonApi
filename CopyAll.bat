cfe1292383d8b1341137fb85e768822b3239ebb4
keshavsoft <nknnkumar@yahoo.com>
2023-06-04 17:26:04 +0530

    keshav

--- CopyAll.bat
+++ CopyAll.bat
@@ -2,15 +2,14 @@ ROBOCOPY ..\JSONDataSupply\DataSupply DataSupply\  /MIR
 
 ROBOCOPY ..\AdminApi\Projects\JSONAdminApi Projects\JSONAdminApi\  /MIR
 
+xcopy ..\StaticWithInclude\public\Projects\JSONAdminApi  public\JSONAdminApi\  /s /e /Y
+xcopy ..\StaticWithInclude\public\Projects\JSONUser  public\JSONUser\  /s /e /Y
+
 xcopy ..\KStaticFiles\public\Projects\ClientLogin public\ClientLogin\  /s /e /Y
-xcopy ..\KStaticFiles\public\Projects\JSONAdminApi  public\JSONAdminApi\  /s /e /Y
 xcopy ..\KStaticFiles\public\Projects\JSONReports  public\JSONReports\  /s /e /Y
-xcopy ..\KStaticFiles\public\Projects\JSONUser  public\JSONUser\  /s /e /Y
 xcopy ..\KStaticFiles\public\Projects\JSONUtility  public\JSONUtility\  /s /e /Y
 
 xcopy ..\KStaticFiles\src\public\ClientLogin public\ClientLogin\  /s /e /Y
-xcopy ..\KStaticFiles\src\public\JSONAdminApi  public\JSONAdminApi\  /s /e /Y
 xcopy ..\KStaticFiles\src\public\JSONReports  public\JSONReports\  /s /e /Y
-xcopy ..\KStaticFiles\src\public\JSONUser  public\JSONUser\  /s /e /Y
 xcopy ..\KStaticFiles\src\public\JSONUtility public\JSONUtility\  /s /e /Y
 xcopy ..\KStaticFiles\src\public\Garments public\Garments\  /s /e /Y
