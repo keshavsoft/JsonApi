5ac4b56d1ea84b6687684153435c4151e3ca755f
keshavsoft <nknnkumar@yahoo.com>
2023-06-05 09:19:53 +0530

    keshav

--- CopyStaticWithInclude.bat
+++ CopyStaticWithInclude.bat
@@ -1,2 +1,3 @@
 xcopy ..\StaticWithInclude\public\Projects\JSONAdminApi  public\JSONAdminApi\  /s /e /Y
 xcopy ..\StaticWithInclude\public\Projects\JSONUser  public\JSONUser\  /s /e /Y
+xcopy ..\StaticWithInclude\public\Projects\JSONApi  public\JSONApi\  /s /e /Y
