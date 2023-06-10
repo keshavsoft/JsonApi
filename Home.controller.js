1eec037322bd108e7a35b9a011a0d160f6cb88c3
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-10 15:47:31 +0530

    keshav

--- Home.controller.js
+++ Home.controller.js
@@ -2,7 +2,7 @@ var path = require('path');
 let CommonHomeScreen = require("./HomeScreen.json");
 
 let GetFunc = (req, res, next) => {
-    let LocalHomeScreen = CommonHomeScreen.HomeHtmlForGarments;
+    let LocalHomeScreen = CommonHomeScreen.HomeHtml;
     res.sendFile(path.join(__dirname + `/public/${LocalHomeScreen}.html`));
 };
 
