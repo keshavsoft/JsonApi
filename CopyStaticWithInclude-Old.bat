47bf6cabba0a679067e013943c9e409ab75a8ca2
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-21 17:45:35 +0530

    Update CopyStaticWithInclude-Old.bat

--- CopyStaticWithInclude-Old.bat
+++ CopyStaticWithInclude-Old.bat
@@ -1,1 +1,5 @@
-xcopy ..\KStaticFiles\public\Projects\JSONAdminApi  public\JSONAdminApi\  /s /e /Y
+xcopy ..\StaticWithInclude\public\Projects\JSONAdminApi  public\JSONAdminApi\  /s /e /Y
+xcopy ..\StaticWithInclude\public\Projects\JSONUser  public\JSONUser\  /s /e /Y
+xcopy ..\StaticWithInclude\public\Projects\JSONApi  public\JSONApi\  /s /e /Y
+xcopy ..\StaticWithInclude\public\Projects\Garments  public\Garments\  /s /e /Y
+xcopy ..\StaticWithInclude\public\Projects\JSONUtility  public\JSONUtility\  /s /e /Y
