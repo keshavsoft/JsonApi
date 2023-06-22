008b40c9263a66340d947dc9e775a4e09f42f852
jatinneeraj <jatinucm123@gmail.com>
2023-06-22 18:28:19 +0530

    jaya

---
+++ CopyStaticWithInclude.sh
@@ -0,0 +1,11 @@
+
+cd ../StaticWithInclude
+node start
+cd ..
+
+cp -r StaticWithInclude/public/Projects/JSONAdminApi/. JsonApi/public/JSONAdminApi
+cp -r StaticWithInclude/public/Projects/JSONUser/. JsonApi/public/JSONUser
+cp -r StaticWithInclude/public/Projects/JSONApi/. JsonApi/public/JSONApi
+cp -r StaticWithInclude/public/Projects/Garments/. JsonApi/public/Garments
+cp -r StaticWithInclude/public/Projects/JSONUtility/. JsonApi/public/JSONUtility
+cp -r StaticWithInclude/public/Projects/JSONReports/. JsonApi/public/JSONReports
