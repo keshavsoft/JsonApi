c4df98be704959cc104d13aa4c6e8b71a1a4b617
keshavsoft <nknnkumar@yahoo.com>
2023-07-08 10:50:44 +0530

    keshav

--- public/JSONApi/Html/UserData/ForCrud/VerticalToShow.js
+++ public/JSONApi/Html/UserData/ForCrud/VerticalToShow.js
@@ -2,6 +2,11 @@ import { jFShowFoldersInMenu } from "../ForDataHtml/Header/FromFolder.js";
 import { StartFunc as VerticalStartFunc } from "./VerticalToShow/FetchFuns.js";
 //import { StartFunc as KeyPressStartFunc } from "./Vertical/AddListenersFuncs/KeyPress.js";
 import { StartFunc as AddListenersFuncsStartFunc } from "./VerticalToShow/AddListenersFuncs/StartFunc.js";
+import { StartFunc as StartFuncClickFuncs } from "../ForDataHtml/Header/ClickFuncs.js";
+import { StartFunc as StartFuncToDataList } from "../ToDataList/FromLocalStorageKey.js";
+
+StartFuncToDataList();
+StartFuncClickFuncs();
 
 jFShowFoldersInMenu({
     inProjectName: jVarGlobalProject,
