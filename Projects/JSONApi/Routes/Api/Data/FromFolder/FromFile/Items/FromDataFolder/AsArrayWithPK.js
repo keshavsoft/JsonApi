e335981e7b23c07f5c95c87193a5518c30be5953
Satish Addanki <satishaddanki321@gmail.com>
2023-07-26 16:39:46 +0530

    satish

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AsArrayWithPK.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AsArrayWithPK.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let router = express.Router();
-//let Repos = require("../../../../../../../Repository/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AsArrayWithPK");
+let Middlewares = require("../../../../../../../Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AsArrayWithPK");
 let CommonConrollers = require("../../../../../../../controllers/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AsArrayWithPK.controller");
 
-router.post('/', CommonConrollers.PostFunc);
+router.post('/', Middlewares.PostFunc, CommonConrollers.PostFunc);
 
 module.exports = router;
