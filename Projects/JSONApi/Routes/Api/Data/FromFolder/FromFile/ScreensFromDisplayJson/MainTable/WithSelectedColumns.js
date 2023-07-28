af98fa001e74e6d7561956b3cf705d8232b84c88
Jaya <darapujayasri507@gmail.com>
2023-07-28 20:45:54 +0530

    keshav

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/MainTable/WithSelectedColumns.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/MainTable/WithSelectedColumns.js
@@ -3,6 +3,6 @@ let router = express.Router();
 let CommonControllers = require("../../../../../../../controllers/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/MainTable/WithSelectedColumns.controllers");
 let CommonMiddleWares = require("../../../../../../../Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/MainTable/WithSelectedColumns");
 
-router.post('/',CommonMiddleWares.WithConfig,CommonControllers.WithConfig)
+router.post('/', CommonMiddleWares.WithConfig, CommonControllers.WithConfig)
 
 module.exports = router;
