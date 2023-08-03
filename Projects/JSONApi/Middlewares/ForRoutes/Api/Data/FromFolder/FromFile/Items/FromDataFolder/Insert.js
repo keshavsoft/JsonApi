c3ae81e54d0e6bb8d4fbb54e4892218464bfc72c
phani <phanindrakumarnakka@gmail.com>
2023-08-03 15:22:08 +0530

    Merge pull request #67 from SatishAddanki96/satish0285 satish

---
+++ Projects/JSONApi/Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/Insert.js
@@ -0,0 +1,36 @@
+exports.PostFunc = (req, res, next) => {
+    if (("DataPk" in req.KeshavSoft) === false) {
+        res.json({ KTF: false, KReason: "DataPk not found in Request" })
+        return;
+    };
+
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                inPostData:"",
+                inJsonConfig:"",
+                inItemConfig: ""
+            },
+        });
+        return;
+    };
+
+    if (("inPostData" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inPostData not found in body" })
+        return;
+    };
+
+    if (("inJsonConfig" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inJsonConfig not found in body" })
+        return;
+    };
+
+    if (("inItemConfig" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inItemConfig not found in body" })
+        return;
+    };
+
+    next();
+};
