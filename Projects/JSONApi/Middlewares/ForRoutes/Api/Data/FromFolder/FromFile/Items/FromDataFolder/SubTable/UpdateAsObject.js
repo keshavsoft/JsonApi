a206833d7f36a12035264eb9ed9e3296f1f55f45
Satish Addanki <satishaddanki321@gmail.com>
2023-08-14 17:35:39 +0530

    satish

---
+++ Projects/JSONApi/Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/SubTable/UpdateAsObject.js
@@ -0,0 +1,45 @@
+exports.WithPK = (req, res, next) => {
+    if (("DataPk" in req.KeshavSoft) === false) {
+        res.json({ KTF: false, KReason: "DataPk not found in Request" })
+        return;
+    };
+
+    if (Object.keys(req.body).length === 0) {
+        res.json({
+            KTF: false,
+            KReason: "post requst body should contain : ",
+            body: {
+                JsonConfig:{},
+                ItemConfig:{}, 
+                inDataToUpdate: {},
+                MainRowPK: "",
+                InsertKey: "",
+            },
+        });
+        return;
+    };
+
+    if (("JsonConfig" in req.body) === false) {
+        res.json({ KTF: false, KReason: "JsonConfig not found in body" })
+        return;
+    };
+
+    if (("ItemConfig" in req.body) === false) {
+        res.json({ KTF: false, KReason: "ItemConfig not found in body" })
+        return;
+    };
+
+    if (("inDataToUpdate" in req.body) === false) {
+        res.json({ KTF: false, KReason: "inDataToUpdate not found in body" })
+        return;
+    };
+    if (("MainRowPK" in req.body) === false) {
+        res.json({ KTF: false, KReason: "MainRowPK not found in body" })
+        return;
+    };
+    if (("InsertKey" in req.body) === false) {
+        res.json({ KTF: false, KReason: "InsertKey not found in body" })
+        return;
+    };
+    next();
+};
