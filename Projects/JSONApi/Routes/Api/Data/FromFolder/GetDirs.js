2bcc1bda1005eb18f0c36621a343d6508de722a7
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-08-15 12:19:30 +0530

    keshav

--- Projects/JSONApi/Routes/Api/Data/FromFolder/GetDirs.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/GetDirs.js
@@ -4,22 +4,19 @@ let Repos = require("../../../../Repository/Api/Data/FromFolder/GetDirs");
 
 router.get('/AsArray', function (req, res, next) {
     let LocalDataPk = req.KeshavSoft.DataPk;
-    console.log("kkkkk : ", req.get('Accept'));
 
     if ((req.get('Accept') === "application/json") === false) {
         res.status(422).send('Accept:application/json only!');
     };
-    console.log("pppppppppp");
-    Repos.AsArray({ inDataPk: LocalDataPk }).then(PromiseData => {
-        //res.json(PromiseData);
 
+    Repos.AsArray({ inDataPk: LocalDataPk }).then(PromiseData => {
         res.end(JSON.stringify(PromiseData));
     });
 });
 
 router.get('/AsTree', function (req, res, next) {
     let LocalDataPk = req.KeshavSoft.DataPk;
-    console.log("AsTree : ");
+
     Repos.AsTree({ inDataPk: LocalDataPk }).then(PromiseData => {
         res.end(JSON.stringify(PromiseData));
     });
