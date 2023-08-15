943befeafa7eeb05c25320ad41c04ec737a7a586
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-08-15 11:37:16 +0530

    keshav

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/SubTable/Delete.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/SubTable/Delete.js
@@ -2,6 +2,6 @@ let express = require('express');
 let router = express.Router();
 let controllers = require("../../../../../../../../controllers/Api/Data/FromFolder/FromFile/Items/FromDataFolder/SubTable/Delete.controller");
 
-router.post('/',controllers.WithPK );
+router.post('/', controllers.WithPK);
 
 module.exports = router;
