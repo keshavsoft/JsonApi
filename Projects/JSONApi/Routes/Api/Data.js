094ba87bc2001c0082826681e688ca14882f108d
Satish Addanki <satishaddanki321@gmail.com>
2023-08-17 14:39:21 +0530

    satish

--- Projects/JSONApi/Routes/Api/Data.js
+++ Projects/JSONApi/Routes/Api/Data.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 let CommonFromFolder = require("./Data/FromFolder");
+let CommonPostQrCodes = require("./Data/PostQrCodes");
 
 router.use('/Masters', require('./Data/Masters'));
 router.use('/FromFolder', CommonFromFolder);
+router.use('/PostQrCodes', CommonPostQrCodes);
 
 module.exports = router;
--- Projects/JSONApi/Routes/Api/Data/PostQrCodes/Maguva.js
+++ Projects/JSONApi/Routes/Api/Data/PostQrCodes/Maguva.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express();
-let Repos = require('../../../../Repository/Api/Data/PostQrCodes/Maguva')
+let Repos = require('../../../../Repository/Api/Data/PostQrCodes/Maguva');
 
 router.get('/:inRowPk', (req, res) => {
     let LocalRowPk = req.params.inRowPk;
