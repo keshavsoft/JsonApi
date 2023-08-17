f1b9b4b8cda9bc76ba369414e400baed43fcaeaa
keshavsoft <nknnkumar@yahoo.com>
2023-08-17 12:45:31 +0530

    keshav

---
+++ Projects/JSONApi/Routes/Api/Data/PostQrCodes/Maguva.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express();
+let Repos = require('../../../../../../../Repository/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Items/Custom/PostFromPk')
+
+router.get('/:inRowPk', (req, res) => {
+    let LocalRowPk = req.params.inRowPk;
+    let LocalDataPk = req.KeshavSoft.DataPk;
+
+    Repos.GetFunc({
+        inClientName: "Maguva",
+        inPurchasePK: LocalRowPk,
+        inDataPk: LocalDataPk
+    }).then(PromiseData => {
+        console.log("PromiseData : ", PromiseData);
+
+        res.json(PromiseData);
+    });
+
+});
+
+module.exports = router;
