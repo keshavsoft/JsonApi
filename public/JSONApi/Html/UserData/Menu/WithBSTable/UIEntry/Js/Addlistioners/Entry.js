c91665942b66a8298f1e790818345e1040a7b2a7
Keshav <nknnkumar@yahoo.com>
2023-09-06 10:00:35 +0530

    keshav

---
+++ public/JSONApi/Html/UserData/Menu/WithBSTable/UIEntry/Js/Addlistioners/Entry.js
@@ -0,0 +1,9 @@
+const StartFunc = () => {
+    let jVarLocalHtmlId = 'localBSTableId';
+    let jVarLocallocalBSTableId = document.getElementById(jVarLocalHtmlId);
+
+    jVarLocallocalBSTableId.addEventListener("click", () => {
+    });
+};
+
+export { StartFunc };
---
+++ public/JSONApi/Html/UserData/Menu/WithBSTable/UIEntry/Js/Options.js
@@ -0,0 +1,10 @@
+import { jFShowFoldersInMenu } from "./FromFolder.js";
+import { StartFunc as BuildCardsFuncsStartFunc } from "./BuildCardsFuncs.js";
+
+jFShowFoldersInMenu({
+    inProjectName: jVarGlobalProject,
+    inSubRoute: jVarGlobalSubRoute
+});
+
+
+// BuildCardsFuncsStartFunc();
