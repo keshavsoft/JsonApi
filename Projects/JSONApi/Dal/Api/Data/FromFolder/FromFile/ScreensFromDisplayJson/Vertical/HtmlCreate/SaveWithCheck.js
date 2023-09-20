bbba01b161f0a87ab234ac04dfd59997f5a0f988
Satish Addanki <satishaddanki321@gmail.com>
2023-09-20 13:57:23 +0530

    satish

--- Projects/JSONApi/Dal/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Vertical/HtmlCreate/SaveWithCheck.js
+++ Projects/JSONApi/Dal/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Vertical/HtmlCreate/SaveWithCheck.js
@@ -1,5 +1,5 @@
 //let CommonDataSupply = require("../../../../../../../../../../DataSupply/Fs/Config/Folders/Files/ConfigFromDisplayJson/Screens/Items/PushData/FromVertical/Save");
-let CommonDataSupply = require("../../../../../../../../../../DataSupply/CommonTableFuncs/FromVertical/Save");
+let CommonDataSupply = require("../../../../../../../../../../DataSupply/CommonTableFuncs/FromVertical/SaveWithCheck");
 
 exports.CheckAndSave = async ({ inJsonConfig, inItemConfig, inDataPK, inPostData }) => {
     let LocalFromCommonDataSupply = await CommonDataSupply.CheckAndSave({ JsonConfig: inJsonConfig, ItemConfig: inItemConfig, inDataPK, inDataToSave: inPostData });
