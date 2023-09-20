1fcf3ec2d6f5e946b9474a17dbce1d1be6c786ee
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-20 13:27:42 +0530

    SREENIVAS

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AggregateFuncs/OnValues.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/AggregateFuncs/OnValues.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 let CommonFilter = require("./OnValues/Filter");
 let CommonGroupBy = require("./OnValues/GroupBy");
+let CommonFilterAsArray = require("./OnValues/FilterAsArray");
 
 router.use('/Filter', CommonFilter);
 router.use('/GroupBy', CommonGroupBy);
+router.use('/FilterAsArray', CommonFilterAsArray);
 
 module.exports = router;
