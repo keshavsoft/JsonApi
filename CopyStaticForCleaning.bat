8873115e9ed09fc01b4e75f16f666f96e24aa537
Satish Addanki <satishaddanki321@gmail.com>
2023-09-26 20:49:43 +0530

    satish

--- CopyStaticForCleaning.bat
+++ CopyStaticForCleaning.bat
@@ -1,7 +1,6 @@
 cd ../StaticWithInclude
 call node Cleaning
 
-xcopy public\Projects\JSONApi  ..\JsonApi\public\JSONApi\  /s /e /Y
 xcopy public\Projects\Cleaning  ..\JsonApi\public\Cleaning\  /s /e /Y
 
 cd ../JsonApi
