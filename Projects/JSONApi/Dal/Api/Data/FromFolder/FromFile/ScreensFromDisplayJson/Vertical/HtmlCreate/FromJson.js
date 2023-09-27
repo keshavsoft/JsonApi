2801e1cfab4d9cfa86d961dff8710137f4249a39
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-27 17:51:04 +0530

    SREENIVAS

--- Projects/JSONApi/Dal/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Vertical/HtmlCreate/FromJson.js
+++ Projects/JSONApi/Dal/Api/Data/FromFolder/FromFile/ScreensFromDisplayJson/Vertical/HtmlCreate/FromJson.js
@@ -1,7 +1,13 @@
 let CommonDataSupply = require("../../../../../../../../../../DataSupply/Fs/Config/Folders/Files/ConfigFromDisplayJson/Screens/Items/PullData/ForVertical/HtmlCreate/Fromjson");
 
 exports.FromJson = async ({ inJsonConfig, inItemConfig, inDataPK }) => {
-    let LocalReturnData = await CommonDataSupply.ShowWithDataPK({ inJsonConfig, inItemConfig, inDataPK });
-    
+    let LocalReturnData = await CommonDataSupply.ShowWithDataPK({
+        inFolderName: inJsonConfig.inFolderName,
+        inFileName: inJsonConfig.inJsonFileName,
+        inItemName: inItemConfig.inItemName,
+        inScreenName: inItemConfig.inScreenName,
+        inDataPK
+    });
+
     return await LocalReturnData;
 };
