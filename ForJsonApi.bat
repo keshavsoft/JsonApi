dfde5bb1625e248ef4546b8b6f0e53b9afb95396
Satish Addanki <satishaddanki321@gmail.com>
2023-10-11 20:26:58 +0530

    satish

---
+++ ForJsonApi.bat
@@ -0,0 +1,6 @@
+cd ../StaticWithInclude
+call node Start
+
+robocopy public\Projects\JSONApi  ..\JsonApi\public\JSONApi\  /MIR
+
+cd ../JsonApi
--- Projects/JSONApi/Dal/Api/Data/FromFolder/FromFile/Items/FromDataFolder/WithScreens/MainTable/SaveRow/WithPkTimeStamp.js
+++ Projects/JSONApi/Dal/Api/Data/FromFolder/FromFile/Items/FromDataFolder/WithScreens/MainTable/SaveRow/WithPkTimeStamp.js
@@ -1,8 +1,7 @@
 // let CommonDataSupply = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PushData/GeneratePk/EntryFile");
+let CommonDataSupply = require("../../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/ConsiderDisplayJson/PushData/GeneratePkWithTimeStamp/EntryFile");
 
 exports.PostFunc = ({ inFolderName, inFileNameOnly, inItemName, inScreenname, inDataPK, inDataToInsert }) => {
-
-    return "From Dal"
 
     let LocalReturnObject = CommonDataSupply.StartFunc({
         inFolderName: inFolderName,
