34d89117a06c2b45d5dc74b1647e6f0238471b0a
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-10-27 15:18:12 +0530

    SREENIVAS

--- Projects/JSONApi/Routes/Api/Data/FromFolder/GetDirs.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/GetDirs.js
@@ -5,9 +5,9 @@ let Repos = require("../../../../Repository/Api/Data/FromFolder/GetDirs");
 router.get('/AsArray', function (req, res, next) {
     let LocalDataPk = req.KeshavSoft.DataPk;
 
-    if ((req.get('Accept') === "application/json") === false) {
-        res.status(422).send('Accept:application/json only!');
-    };
+    // if ((req.get('Accept') === "application/json") === false) {
+    //     res.status(422).send('Accept:application/json only!');
+    // };
 
     Repos.AsArray({ inDataPk: LocalDataPk }).then(PromiseData => {
         res.end(JSON.stringify(PromiseData));
