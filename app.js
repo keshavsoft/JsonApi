f868eaf96d9e91a2db96dcd81f00afac4eddd125
Satish Addanki <satishaddanki321@gmail.com>
2023-11-20 11:51:06 +0530

    satish

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let CommonProjectNameForJSONAdminApi = "JSONAdminApi";
 let CommonProjectNameForJSONUtility = "JSONUtility";
 
 let Commoncontrollers = require("./controllers/missedAll.controller");
-let CommonForWebSocketStart = require("./Projects/ForWebSocket/EntryFile");
+let CommonForWebSocketStart = require("./Projects/KWSServer/EntryFile");
 let CommonHomeController = require("./StartUp/Home.controller");
 
 require('dotenv').config()
