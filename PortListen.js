41d12c18ea8517e129a52ca3f4182eeb79bad2d1
Keshav <nknnkumar@yahoo.com>
2023-12-04 16:23:30 +0530

    keshav

--- PortListen.js
+++ PortListen.js
@@ -1,24 +1,26 @@
-let CommonChatFolder = require("./DataSupply/Fs/Config/JSONFolder/CommonChatFolder/JsonFile/PushDataToFile/CreateFileOnly");
+// let CommonChatFolder = require("./DataSupply/Fs/Config/JSONFolder/CommonChatFolder/JsonFile/PushDataToFile/CreateFileOnly");
 
 let StartFunc = () => {
+    console.log(`Listening in some local port...`);
+
     // console.log(`Listening in port : ${port}`);
     // console.log(`Click to open : http://localhost:${port}`);
     process.env.UUID = uuidv4();
-    LocalFuncToWebSocketAsFile();
+//    LocalFuncToWebSocketAsFile();
 };
 
-let LocalFuncToWebSocketAsFile = () => {
-    let LocalFolderName = "ForChat";
-    //let LocalFileNameOnly = "ConnectedClients";
+// let LocalFuncToWebSocketAsFile = () => {
+//     let LocalFolderName = "ForChat";
+//     //let LocalFileNameOnly = "ConnectedClients";
 
-    let LocalFileNameOnly = process.env.UUID;
+//     let LocalFileNameOnly = process.env.UUID;
 
-    // let LocalItemName = process.env.UUID;
+//     // let LocalItemName = process.env.UUID;
 
-    CommonChatFolder.StartFunc({
-        inFileNameOnly: LocalFileNameOnly
-    });
-};
+//     CommonChatFolder.StartFunc({
+//         inFileNameOnly: LocalFileNameOnly
+//     });
+// };
 
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
