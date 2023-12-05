64513ca213a3a6ec19996af9bb4a604f370517a6
Keshav <nknnkumar@yahoo.com>
2023-12-05 15:57:25 +0530

    keshav

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/MainTable/SaveRow/WithPkTSImage.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Items/FromDataFolder/MainTable/SaveRow/WithPkTSImage.js
@@ -2,12 +2,12 @@ let express = require("express");
 let router = express.Router();
 
 let DataSupply = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PushData/WithPkTSImage/UsingMulter");
-let DataSupplyCheckImage = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PushData/WithPkTSImage/CheckImage/InJsonDataFolder");
+// let DataSupplyCheckImage = require("../../../../../../../../../../../DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PushData/WithPkTSImage/CheckImage/InJsonDataFolder");
 
-router.post('/', DataSupply.upload.single("uploaded_file"), (req, res) => {
-    let LocalFromDataSupply = DataSupplyCheckImage.StartFunc({ req });
+router.post('/', DataSupply.upload.single("image"), (req, res) => {
+    // let LocalFromDataSupply = DataSupplyCheckImage.StartFunc({ req });
 
-    res.json(LocalFromDataSupply);
+    res.json(req.FromDataSupply);
 });
 
 module.exports = router;
