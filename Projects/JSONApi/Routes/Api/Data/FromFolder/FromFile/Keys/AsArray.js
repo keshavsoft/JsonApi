8dc1d293aa42eddf26b32580987258ca35e54f6c
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-12-23 13:06:06 +0530

    SREENIVAS

--- Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Keys/AsArray.js
+++ Projects/JSONApi/Routes/Api/Data/FromFolder/FromFile/Keys/AsArray.js
@@ -1,7 +1,8 @@
 let express = require('express');
 let router = express.Router();
 let CommonController = require("../../../../../../controllers/Api/Data/FromFolder/FromFile/Keys/getData.controller");
+let CommonMiddlewares = require("../../../../../../Middlewares/ForRoutes/Api/Data/FromFolder/FromFile/Keys/AsArray");
 
-router.post('/', CommonController.PostFunc)
+router.post('/', CommonMiddlewares.PostFunc, CommonController.PostFunc)
 
 module.exports = router;
--- Projects/JSONApi/controllers/Api/Data/FromFolder/FromFile/Keys/getData.controller.js
+++ Projects/JSONApi/controllers/Api/Data/FromFolder/FromFile/Keys/getData.controller.js
@@ -1,41 +1,17 @@
-//let Repos = require("../../../../../../../Repository/Api/Data/FromFolder/FromFile/Items/FromDataFolder/Insert");
 let Repos = require("../../../../../../Repository/Api/Data/FromFolder/FromFile/Keys/GetData");
 
-let PostFunc = (req, res, next) => {
-    let LocalDataPK;
-    let LocalFolderName;
-    let LocalFileName;
+let PostFunc = (req, res) => {
 
-    if ("KeshavSoft" in req) {
-        if ("DataPk" in req.KeshavSoft) {
-            LocalDataPK = req.KeshavSoft.DataPk;
+    let LocalDataPK = req.KeshavSoft.DataPk;
+    let LocalFolderName = req.body.inFolderName;
+    let LocalFileName = req.body.inFileNameOnly;
 
-            if ("inFolderName" in req.body) {
-                LocalFolderName = req.body.inFolderName;
-
-                if ("inFileNameOnly" in req.body) {
-                    LocalFileName = req.body.inFileNameOnly;
-
-                    let PromiseData = Repos.PostFunc({
-                        inFolderName: LocalFolderName,
-                        inFileNameOnly: LocalFileName,
-                        inDataPK: LocalDataPK
-                    });
-
-                    res.json(PromiseData);
-
-                } else {
-                    res.json({ KTF: false, KReason: "Need to send inFileNameWithExtension" });
-                };
-            } else {
-                res.json({ KTF: false, KReason: "Need to send inFolderName" });
-            };
-        } else {
-            res.json({ KTF: false, KReason: "DataPk not found in Request.KeshavSoft" });
-        };
-    } else {
-        res.json({ KTF: false, KReason: "KeshavSoft not found in Request" });
-    };
+    let PromiseData = Repos.PostFunc({
+        inFolderName: LocalFolderName,
+        inFileNameOnly: LocalFileName,
+        inDataPK: LocalDataPK
+    });
+    res.status(200).json(PromiseData);
 };
 
 module.exports = {
